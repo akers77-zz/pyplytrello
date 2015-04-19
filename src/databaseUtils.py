a7a498e713a104390ce717db5e41441ec160ad0c
Matt Akerman <m.ak@me.com>
2015-04-19 07:20:08 +0100

    convertURIsToMetadataStrings implemented - takes an array of Spotify URIs and produces an array of strings with the song title and artist

--- src/databaseUtils.py
+++ src/databaseUtils.py
@@ -1,11 +1,16 @@
 __author__ = 'matt'
 
-import peewee
 from databaseAccess import *
+import requests
+
+ECHONEST_API_KEY="S0QUTXZNEZ6GNVKST"
+BASE_URL="http://developer.echonest.com/api/v4/song/profile?api_key="
+POST_URL="&format=json&bucket=id:spotify&track_id="
 
 class DatabaseUtils:
-    def getCurrentPlaylist(self, partyID):
-        playlist = Playlist.select().where(Playlist.partyId == partyID).order_by(Playlist.votes)
+
+    def getCurrentPlaylistURIs(self, partyID):
+        playlist = Playlist.select().where(Playlist.partyId == partyID).order_by(Playlist.votes.desc())
 
         spotifyURIs = []
 
@@ -13,3 +18,23 @@ class DatabaseUtils:
             spotifyURIs.append(song.spotifyId)
 
         return spotifyURIs
+
+    def convertURIsToMetadataString(self, spotifyURIs):
+        stringArray = []
+
+        for uri in spotifyURIs:
+            r = requests.get('{}{}{}{}'.format(BASE_URL,ECHONEST_API_KEY,POST_URL,uri))
+            if r.status_code == 200:
+                response = r.json()['response']
+                if response['songs']:
+                    songs = response['songs']
+                    artist = songs[0]['artist_name']
+                    title = songs[0]['title']
+                    stringArray.append(title + " by " + artist)
+
+        return stringArray
+
+#db = DatabaseUtils()
+#uris = db.getCurrentPlaylistURIs("1234")
+#strings = db.convertURIsToMetadataString(uris)
+#print strings
